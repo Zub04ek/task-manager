e476f24003dd8f8444a0d7d16e5d9984ef9a7378
Nastya <nastasyz@ukr.net>
2024-10-23 17:29:29 +0300

    configuring getting and posting tasks to database

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,97 +1,9 @@
-import { AddTaskDialog } from '@/components/AddTaskDialog';
-import { TaskCard } from '@/components/TaskCard';
-
-const GROUPS = [
-  {
-    title: 'To do',
-  },
-  {
-    title: 'In progress',
-  },
-  {
-    title: 'Done',
-  },
-];
-
-const todos = [
-  {
-    userId: 1,
-    id: 1,
-    title: 'delectus aut autem',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia explicabo alias harum officia porro, ab deserunt recusandae in, ut nulla magnam accusantium exercitationem, ullam cumque ea et. Recusandae temporibus nobis vitae dignissimos ab blanditiis corporis obcaecati necessitatibus explicabo quis facere commodi esse, numquam sequi possimus ex error quae mollitia.',
-    status: 'to do',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: 'quis ut nam facilis et officia qui',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia explicabo alias harum officia porro, ab deserunt recusandae in, ut nulla magnam accusantium exercitationem, ullam cumque ea et. Recusandae temporibus nobis vitae dignissimos ab blanditiis corporis obcaecati necessitatibus explicabo quis facere commodi esse, numquam sequi possimus ex error quae mollitia.',
-    status: 'in progress',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: 'fugiat veniam minus',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia explicabo alias harum officia porro, ab deserunt recusandae in, ut nulla magnam accusantium exercitationem, ullam cumque ea et. Recusandae temporibus nobis vitae dignissimos ab blanditiis corporis obcaecati necessitatibus explicabo quis facere commodi esse, numquam sequi possimus ex error quae mollitia.',
-    status: 'completed',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 4,
-    title: 'et porro tempora',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia explicabo alias harum officia porro, ab deserunt recusandae in, ut nulla magnam accusantium exercitationem, ullam cumque ea et. Recusandae temporibus nobis vitae dignissimos ab blanditiis corporis obcaecati necessitatibus explicabo quis facere commodi esse, numquam sequi possimus ex error quae mollitia.',
-    status: 'to do',
-    completed: true,
-  },
-  {
-    userId: 1,
-    id: 5,
-    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia explicabo alias harum officia porro, ab deserunt recusandae in, ut nulla magnam accusantium exercitationem, ullam cumque ea et. Recusandae temporibus nobis vitae dignissimos ab blanditiis corporis obcaecati necessitatibus explicabo quis facere commodi esse, numquam sequi possimus ex error quae mollitia.',
-    status: 'in progress',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 6,
-    title: 'qui ullam ratione quibusdam voluptatem quia omnis',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia explicabo alias harum officia porro, ab deserunt recusandae in, ut nulla magnam accusantium exercitationem, ullam cumque ea et. Recusandae temporibus nobis vitae dignissimos ab blanditiis corporis obcaecati necessitatibus explicabo quis facere commodi esse, numquam sequi possimus ex error quae mollitia.',
-    status: 'completed',
-    completed: false,
-  },
-];
+import { TasksList } from '@/components/TasksList';
 
 export default function Home() {
   return (
     <div className="grid h-full gap-6 lg:grid-cols-3">
-      {GROUPS.map((group, index) => {
-        return (
-          <div
-            key={index}
-            className="grid grid-rows-[36px_1fr] gap-6 rounded-xl bg-accent py-6 pl-6 pr-3"
-          >
-            <div className="flex items-center justify-between pr-3">
-              <span className="font-semibold">{group.title}</span>
-
-              {group.title === 'To do' && <AddTaskDialog />}
-            </div>
-            <ul className="flex h-[667px] flex-col gap-6 overflow-y-auto scroll-smooth pr-3">
-              {todos.map((todo) => {
-                return <TaskCard key={todo.id} task={todo} />;
-              })}
-            </ul>
-          </div>
-        );
-      })}
+      <TasksList />
     </div>
   );
 }
---
+++ app/api/tasks/route.ts
@@ -0,0 +1,93 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import prisma from '@/lib/prisma';
+import { Task } from '@prisma/client';
+
+export async function POST(req: NextRequest) {
+  try {
+    // const { userId } = auth();
+
+    // if (!userId) {
+    //   return NextResponse.json({ error: "Unauthorized", status: 401 });
+    // }
+
+    const { title, description, tags, priority } = await req.json();
+
+    if (!title || !description || !tags || !priority) {
+      return NextResponse.json({
+        error: 'Missing required fields',
+        status: 400,
+      });
+    }
+
+    if (title.length < 3) {
+      return NextResponse.json({
+        error: 'Title must be at least 3 characters long',
+        status: 400,
+      });
+    }
+
+    const task = await prisma.task.create({
+      data: <Task>{
+        title,
+        description,
+        tags,
+        priority,
+      },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log('ERROR CREATING TASK: ', error);
+    return NextResponse.json({ error: 'Error creating task', status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    // const { userId } = auth();
+
+    // if (!userId) {
+    //   return NextResponse.json({ error: "Unauthorized", status: 401 });
+    // }
+
+    const tasks = await prisma.task.findMany({
+      orderBy: { createdAt: 'desc' },
+    });
+    //   {where: {
+    //     userId,
+    //   },}
+
+    console.log('task :>> ', tasks);
+
+    return NextResponse.json(tasks);
+  } catch (error) {
+    console.log('ERROR GETTING TASKS: ', error);
+    return NextResponse.json({ error: 'Error getting task', status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    // const { userId } = auth();
+    const { isCompleted, id } = await req.json();
+
+    // if (!userId) {
+    //   return NextResponse.json({ error: "Unauthorized", status: 401 });
+    // }
+
+    const task = await prisma.task.update({
+      where: {
+        id,
+      },
+      data: {
+        isCompleted,
+      },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log('ERROR UPDATING TASK: ', error);
+    return NextResponse.json({ error: 'Error updating task', status: 500 });
+  }
+}
